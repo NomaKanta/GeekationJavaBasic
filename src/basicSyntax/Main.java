7bef9065ec6441325a6ab7f8e8ae4e4322d0391c
NomaKanta <k.noma.cingroup2012@gmail.com>
2024-07-15 20:47:51 +0900

    クラス課題提出

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,12 @@
+package basicSyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        int myAge = 25;
+        double myHeight = 1.75;
+
+        GeekIntroduction.greeting(myAge, myHeight);
+        GeekIntroduction.specialSkill(10, 50);
+    }
+    
+}
