fd471c47eb3e73c7414709756baab1e6a41036a9
NomaKanta <k.noma.cingroup2012@gmail.com>
2024-07-15 17:57:25 +0900

    メソッド課題提出

--- src/basicSyntax/MethodPractice.java
+++ src/basicSyntax/MethodPractice.java
@@ -5,44 +5,37 @@ public class MethodPractice {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		int a = 10;
-		int b = 5;
+	    int b = 5;
 
-		// メソッド1〜4の計算結果を順番に出力
-		int sumResult = add(a, b);
-		int subtractResult = subtract(a, b);
-		int multiplyResult = multiply(a, b);
-		int divideResult = divide(a, b);
+	    int addUpResult = addUp(a,b);
+	    int subtractResult = subtract(a,b);
+	    int multiplyResult = multiply(a,b);
+	    int divideResult = divide(a,b);
 
-		// メソッド5を使って計算結果を出力
-		printResult(sumResult);
-		printResult(subtractResult);
-		printResult(multiplyResult);
-		printResult(divideResult);
-	}
+	    System.out.println("足し算の計算結果は" + addUpResult + "です。");
+	    System.out.println("引き算の計算結果は" + subtractResult + "です。");
+	    System.out.println("掛け算の計算結果は" + multiplyResult + "です。");
+	    System.out.println("割り算の計算結果は" + divideResult + "です。");
+	  }
 
-	// 二つの値を足し算するメソッド
-	private static int add(int a, int b) {
-		return a + b;
-	}
+	  // 足し算をするメソッド
+	  public static int addUp(int a, int b) {
+	    return a + b;
+	  }
 
-	// 二つの値を引き算するメソッド
-	private static int subtract(int a, int b) {
-		return a - b;
-	}
+	  // 引き算をするメソッド
+	  public static int subtract(int a, int b) {
+	    return a - b;
+	  }
 
-	// 二つの値を掛け算するメソッド
-	private static int multiply(int a, int b) {
-		return a * b;
-	}
+	  // 掛け算をするメソッド
+	  public static int multiply(int a, int b) {
+	    return a * b;
+	  }
 
-	// 二つの値を割り算するメソッド
-	private static int divide(int a, int b) {
-		return a / b;
-	}
-
-	// コンソールに出力するためのメソッド
-	private static void printResult(int result) {
-		System.out.println("計算結果は" + result + "です。");
-	}
-
+	  // 割り算をするメソッド
+	  public static int divide(int a, int b) {
+	    return a / b;
+	  }
+	
 }
