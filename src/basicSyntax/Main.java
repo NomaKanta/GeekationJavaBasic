20bd33366592330697a5490f090854914e5be84b
NomaKanta <k.noma.cingroup2012@gmail.com>
2024-07-20 12:51:54 +0900

    修正後再提出

--- src/basicSyntax/Main.java
+++ src/basicSyntax/Main.java
@@ -1,12 +1,13 @@
 package basicSyntax;
 
 public class Main {
-    public static void main(String[] args) {
-        int myAge = 25;
-        double myHeight = 1.75;
-
-        GeekIntroduction.greeting(myAge, myHeight);
-        GeekIntroduction.specialSkill(10, 50);
-    }
-    
+	public static void main(String[] args) {
+		int myAge = 25;
+		double myHeight = 1.75;
+		GeekIntroduction.greeting(myAge, myHeight);
+		
+		int num1 = 10;
+		int num2 = 20;
+		GeekIntroduction.specialSkill(num1, num2);
+	}
 }
